a6948c2a17d5af0de2e0c9df931b0be586948a2b
James Dindi <dindijjames@gmail.com>
2026-06-06 14:27:36 +0300

    Add login endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
+      post 'login', to: 'sessions#create'
+
       resources :geolocations, only: [ :create, :destroy ] do
         collection do
           get :lookup
